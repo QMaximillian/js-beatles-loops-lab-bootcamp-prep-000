354b3755943e45557d005a588f90abf097d218f2
Quinn Lashinsky <quinnlashinsky@gmail.com>
2017-11-25 19:28:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians, instruments) {
+var newArray = [];
+
+for (let i = 0; i < musicians.length; i++) {
+  newArray.push(musicians[i] + " plays " + instruments[i])
+}
+
+return newArray
+}
+
+
+function johnLennonFacts(facts){
+  while (facts.length > 0) {
+    return facts
+
+  }
+}
